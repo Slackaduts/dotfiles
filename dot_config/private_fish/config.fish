f7344f79fc05e9fb3a757815cde3f590b801e7c7
slackaduts <54456755+Slackaduts@users.noreply.github.com>
2023-03-26 23:42:53 -0400

    Initial push

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,9 @@
+#if status is-interactive
+    # Commands to run in interactive sessions can go here
+#end
+
+set fish_greeting ""
+starship init fish | source
+clear
+#fm6000 -f .config/ascii_art/eevee.txt -c green -o "EndeavorOS"
+.local/bin/diyfetch
