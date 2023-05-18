4fb25a3125b02d1782de43622d665f09dae37e98
slackaduts <54456755+Slackaduts@users.noreply.github.com>
2023-05-17 20:49:42 -0400

    Update .config/hypr/scripts/screenrecord.sh

--- dot_config/hypr/scripts/screenrecord.sh
+++ dot_config/hypr/scripts/screenrecord.sh
@@ -9,5 +9,5 @@ if [ -f "$flag_file" ]; then
   rm "$flag_file"
 else
   echo "Starting recording..."
-  wf-recorder --audio=alsa_output.pci-0000_0f_00.4.iec958-stereo.monitor --device=/dev/dri/renderD128 -F fps=60 --codec=h264_qsv -p b=3.5M -g "$(slurp)" -f "$flag_file" & echo $! >> "$flag_file"
+  wf-recorder --audio=alsa_output.pci-0000_0f_00.4.iec958-stereo.monitor --device=/dev/dri/renderD128 -F fps=60 --codec=h264_qsv -p b=3.5M -g "$(slurp)" -f "$flag_file" && echo $! >> "$flag_file"
 exit 0
