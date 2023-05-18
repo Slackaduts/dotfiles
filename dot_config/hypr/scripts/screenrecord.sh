b6e68b6ea358c90db20091e8c472da99d242f39f
slackaduts <54456755+Slackaduts@users.noreply.github.com>
2023-05-17 20:47:31 -0400

    Update .config/hypr/scripts/screenrecord.sh

--- dot_config/hypr/scripts/screenrecord.sh
+++ dot_config/hypr/scripts/screenrecord.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-
 timestamp=$(date "+%Y.%m.%d-%H.%M.%S")
+file="~/Videos/${timestamp}.mp4"
 flag_file="/tmp/recording.flag"
 if [ -f "$flag_file" ]; then
   echo "Recording is already in progress. Stopping..."
   pid=$(cat "$flag_file")
-  kill "$pid"
+  pkill "$pid"
   rm "$flag_file"
 else
   echo "Starting recording..."
-  wf-recorder --audio=alsa_output.pci-0000_0f_00.4.iec958-stereo.monitor --device=/dev/dri/renderD128 -F fps=60 --codec=h264_qsv -p b=3.5M -g "$(slurp)" -f ~/Videos/$(date "+%Y.%m.%d-%H.%M.%S").mp4 & echo $! >> "$flag_file"
+  wf-recorder --audio=alsa_output.pci-0000_0f_00.4.iec958-stereo.monitor --device=/dev/dri/renderD128 -F fps=60 --codec=h264_qsv -p b=3.5M -g "$(slurp)" -f "$flag_file" & echo $! >> "$flag_file"
 exit 0
